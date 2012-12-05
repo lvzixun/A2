3a0d0049e4f9e5e623c9f2df1eeced575fad5861
lvzixun <lvzixun@gmail.com>
2012-12-05 22:55:37 +0800

    Revert "alex 解释器2.0版本"
    This reverts commit 689d1eff6467d9576f7ad6103b82e553d5a833dc.

--- a2_error.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdarg.h>
-
-void a2_error(const char* f, ...){
-	va_list args;
-	va_start(args, f);
-	printf(f, args);
-	va_end(args);
-}
--- a2_error.h
+++
@@ -1,5 +0,0 @@
-#ifndef _A2_ERROR_H_
-#define _A2_ERROR_H_
-
-void a2_error(const char* f, ...);
-#endif
