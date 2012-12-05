689d1eff6467d9576f7ad6103b82e553d5a833dc
lvzixu <lvzixun@gmail.com>
2012-12-05 22:33:35 +0800

    alex 解释器2.0版本
    self

---
+++ a2_error.h
@@ -0,0 +1,5 @@
+#ifndef _A2_ERROR_H_
+#define _A2_ERROR_H_
+
+void a2_error(const char* f, ...);
+#endif
---
+++ test/test_io.c
@@ -0,0 +1,16 @@
+#include "../a2_io.h"
+#include "../a2_conf.h"
+#include <stdio.h>
+
+
+int main(void){
+	struct a2_io*  p = a2_io_open("./tfile");
+	printf("----file-------\n");
+	while(!a2_io_end(p)){
+		printf("%c", a2_io_readchar(p));
+	}
+
+	a2_io_close(p);
+	mem_print();
+	return 0;
+}
